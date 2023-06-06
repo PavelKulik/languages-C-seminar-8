20f176d4c22d82addae4ac3ecb1aaf297bf395b4
Pavel Kulik <Kanstantin0105@gmail.com>
2023-06-06 20:45:53 +0300

    task_1 add

---
+++ task_1/Program.cs
@@ -0,0 +1,53 @@
+﻿int[,] GetArray(int row, int col, int valueMin, int valueMax)
+{
+    int[,] array = new int[row, col];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(valueMin, valueMax);
+        }
+    }
+
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($" {array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int [,] SortArrayRows(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = j + 1; k < array.GetLength(1); k++)
+            {
+                if (array[i, j] < array[i, k])
+                {
+                    int temp = array[i, j];
+                    array[i, j] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+
+    return array;
+}
+
+int[,] array = GetArray(3, 4, 1, 10);
+Console.WriteLine("Массив:");
+PrintArray(array);
+int[,] sortArray = SortArrayRows(array);
+Console.WriteLine("Сортировка строк массива:");
+PrintArray(array);
